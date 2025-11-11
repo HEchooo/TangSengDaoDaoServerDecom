3d4fdd868d0db6b414fcdce5cc767678551aaedf
sofent <sofent@qq.com>
2025-11-11 19:59:43 +0800

    fix

--- modules/common/sql/common-20251111-01.sql
+++ modules/common/sql/common-20251111-01.sql
@@ -1,4 +1,4 @@
 -- +migrate Up
 
 -- 添加租户欢迎语配置字段
-ALTER TABLE `app_config` ADD COLUMN `welcome_message_tenant` TEXT NOT NULL DEFAULT '{}' COMMENT '租户欢迎语配置(JSON格式,key是tenantCode_language,如T18002704_zh_CN,value是welcomeMessage)';
+ALTER TABLE `app_config` ADD COLUMN `welcome_message_tenant` TEXT DEFAULT NULL COMMENT '租户欢迎语配置(JSON格式,key是tenantCode_language,如T18002704_zh_CN,value是welcomeMessage)';
